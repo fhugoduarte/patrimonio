02169cf0d77752519abcae296db2a5f2d407b58b
fhugoduarte <h.duarte222@gmail.com>
2018-02-22 19:47:44 -0300

    Corrigindo alguns detalhes na api

--- patrimonio-api/src/main/java/com/curso/patrimonio/resource/ItemResource.java
+++ patrimonio-api/src/main/java/com/curso/patrimonio/resource/ItemResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.curso.patrimonio.model.Item;
 import com.curso.patrimonio.repository.ItemRepository;
 
 @RestController
+@CrossOrigin("${origem-permitida}")
 public class ItemResource {
 
 	@Autowired
